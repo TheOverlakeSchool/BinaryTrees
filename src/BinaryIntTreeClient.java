140d60790716041a2ef98e50394c677c1592d61a
zerbash <dbasham@overlake.org>
2023-11-07 14:56:49 -0800

    11-7-23

---
+++ src/BinaryIntTreeClient.java
@@ -0,0 +1,7 @@
+public class BinaryIntTreeClient {
+    public static void main(String[] args) {
+        BinaryIntTree tree = new BinaryIntTree();
+        tree.print();
+        System.out.println(tree.contains(42));
+    }
+}
